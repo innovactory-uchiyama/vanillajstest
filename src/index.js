9dc83481a58f097484a5d860d746b87481fe7081
DESKTOP-LF1KO60\lotus <lotus0033@gmai.com>
2021-07-29 16:03:46 +0900

    upate

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-import { buttonClick } from "./js/part_of_js";
+// import { buttonClick } from "./js/part_of_js";
 import "./css/button.scss"
 
 console.log('loaded js')
+
+var count = -1
+var counter = document.createElement('p');
+counter.textContent = count.toString()
+
+const increment = () => {
+    count = count + 1
+    counter.textContent = (count)
+}
+
+const root = document.getElementsByClassName("root")[0]
+if(root) {
+    increment()
+    root.appendChild(counter)
+}
 
 const elements = document.getElementsByClassName("decorate-button");
 
 for (let index = 0; index < elements.length; index++) {
     const element = elements[index];
-    element.addEventListener("click", buttonClick);
+    element.addEventListener("click", increment);
 }
